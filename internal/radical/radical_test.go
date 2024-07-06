22a30d2793a0c911b465a7721bdfbd3e704226b4
Blake Williams <blake@blakewilliams.me>
2024-07-06 11:53:58 -0400

    Cover and handle edgecases in radical

--- internal/radical/radical_test.go
+++ internal/radical/radical_test.go
@@ -45,6 +45,29 @@ func TestNode_Dynamic(t *testing.T) {
 	require.Equal(t, 1, value)
 }
 
+func TestNode_DynamicEdgeCases(t *testing.T) {
+	root := radical.New[int]()
+
+	// Simulate /foo/bar/baz
+	root.Add([]string{"foo", ":bar", "baz"}, 1)
+
+	// duplicate routes should panic
+	require.Panics(t, func() {
+		root.Add([]string{"foo", ":bar", "baz"}, 2)
+	})
+
+	ok, value := root.Value([]string{"foo", "bar", "baz"})
+	require.True(t, ok)
+	require.Equal(t, 1, value)
+
+	// Add route that overlaps with different named parameter
+	root.Add([]string{"foo", ":bar", "baz", "quux"}, 3)
+
+	ok, value = root.Value([]string{"foo", "anything", "baz", "quux"})
+	require.True(t, ok)
+	require.Equal(t, 3, value)
+}
+
 func TestNode_Backpedal(t *testing.T) {
 	root := radical.New[int]()
 
